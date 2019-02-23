4c1b3befdd49d1bff3de4ad6d10dfd87a48aa408
chong yang <chongyang1995@gmail.com>
2019-02-23 21:53:42 +0000

    hardcode the data for frontend

--- app.js
+++ app.js
@@ -28,6 +28,43 @@ var server = http.createServer(function(request, response){
     response.setHeader('Content-Type', 'text/html');
     response.end('<h1>Hello World</h1>');
 
+  }else if(path == '/page2.html'){
+    response.writeHead(200, {
+    'Content-Type': 'text/plain',
+    'Access-Control-Allow-Origin' : '*',
+    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE'
+    });
+    let fake_data = [
+      {
+        "avatar": "https://institutogoldenprana.com.br/wp-content/uploads/2015/08/no-avatar-25359d55aa3c93ab3466622fd2ce712d1.jpg",
+        "alt": "alt",
+        "title": "Donald Trump",
+        "userId": 1,
+        "subtitle" : "I'm gonna build a wall",
+        "date": "2014-08-04T00:00:00Z",
+        "unread": 0
+      },
+      {
+        "avatar": "https://institutogoldenprana.com.br/wp-content/uploads/2015/08/no-avatar-25359d55aa3c93ab3466622fd2ce712d1.jpg",
+        "alt": "alt",
+        "title": "Jens Krinke",
+        "userId": 2,
+        "subtitle" : "Are there any more questions????",
+        "date": "2014-08-04T00:00:00Z",
+        "unread": 0
+      },
+      {
+        "avatar": "https://institutogoldenprana.com.br/wp-content/uploads/2015/08/no-avatar-25359d55aa3c93ab3466622fd2ce712d1.jpg",
+        "alt": "alt",
+        "title": "Graham Collins",
+        "userId": 3,
+        "subtitle" : "Win this book in 5 simple steps!",
+        "date": "2014-08-04T00:00:00Z",
+        "unread": 3
+      }
+    ]
+    response.write(JSON.stringify(fake_data));
+    response.end()
   }else if(path == '/page3'){
     response.setHeader('Content-Type', 'text/html; charset=utf-8')
     response.write('<!DOCTYPE>\n<html>'  + 
