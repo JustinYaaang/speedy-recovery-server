e51e05904a2da0c93ee813627ecd1f94acd9908d
chong yang <chongyang1995@gmail.com>
2019-02-25 00:03:44 +0000

    test v5

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const port=process.env.PORT || 3000
 var url = require('url')
 var fs = require('fs')
 var mysql = require('mysql');
+var authData = JSON.parse(fs.readFileSync('./auth_folder/azure_auth.json', 'utf8'))[0];
 
 
 // const server = http.createServer((req, res) => {
@@ -37,37 +38,66 @@ var server = http.createServer(function(request, response){
     'Access-Control-Allow-Origin' : '*',
     'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE'
     });
-    let fake_data = [
-      {
-        "avatar": "https://institutogoldenprana.com.br/wp-content/uploads/2015/08/no-avatar-25359d55aa3c93ab3466622fd2ce712d1.jpg",
-        "alt": "alt",
-        "title": "Donald Trump",
-        "userId": 1,
-        "subtitle" : "I'm gonna build a wall",
-        "date": "2014-08-04T00:00:00Z",
-        "unread": 0
-      },
-      {
-        "avatar": "https://institutogoldenprana.com.br/wp-content/uploads/2015/08/no-avatar-25359d55aa3c93ab3466622fd2ce712d1.jpg",
-        "alt": "alt",
-        "title": "Jens Krinke",
-        "userId": 2,
-        "subtitle" : "Are there any more questions????",
-        "date": "2014-08-04T00:00:00Z",
-        "unread": 0
-      },
-      {
-        "avatar": "https://institutogoldenprana.com.br/wp-content/uploads/2015/08/no-avatar-25359d55aa3c93ab3466622fd2ce712d1.jpg",
-        "alt": "alt",
-        "title": "Graham Collins",
-        "userId": 3,
-        "subtitle" : "Win this book in 5 simple steps!",
-        "date": "2014-08-04T00:00:00Z",
-        "unread": 3
+
+    var connection = mysql.createConnection({
+      host: authData.host,
+      port: 3306,
+      user: authData.user,
+      password: authData.password,
+      database: authData.database,
+      ssl: fs.readFileSync(authData.ssl_file),
+      insecureAuth: true
+    });
+
+    connection.connect(function(err) {
+      if (err){
+        console.log("error when connecting")
+        console.log(err)
       }
-    ]
-    response.write(JSON.stringify(fake_data));
-    response.end()
+      else{
+        console.log("successfully connecting")        
+      }
+    });
+
+    let target_user = '\'user1\''
+    var select_sql = "SELECT * FROM messages WHERE (sender = " + target_user + " or recipient = " + target_user + ") AND NOT EXISTS (SELECT * FROM messages as M2 WHERE M2.Conversation_Id = messages.Conversation_Id AND M2.Id > messages.Id) ORDER BY time"
+    var ans = [];
+
+    connection.query(select_sql, function (error, results, fields) {
+        if (error){
+            console.log("error when selecting")
+            console.log(error)
+        }
+        else{
+            console.log('The answer is: ', results);
+            for (var i = 0; i < results.length; i++){
+              var username = results[i].Sender;
+              if ("\'" + username + "\'" == target_user){
+                username = results[i].Recipient;
+              }
+              ans.push({"alt":"alt",
+                'title': username,
+                'subtitle': results[i].Message,
+                'date': results[i].time,
+                'unread': 2
+              });
+
+            }
+            console.log(ans);
+        }
+    });
+
+    connection.end(function(err) {
+      if (err){
+          console.log("error when disconnectiong")
+      }
+      else{
+          console.log("successfully disconnectiong")        
+      }
+      // response.write(fs.readFileSync("./test_files/conversations.json", 'utf8'));
+      response.write(JSON.stringify(ans));
+      response.end()
+    });
   }else if(path == '/page3'){
     response.setHeader('Content-Type', 'text/html; charset=utf-8')
     response.write('<!DOCTYPE>\n<html>'  + 
